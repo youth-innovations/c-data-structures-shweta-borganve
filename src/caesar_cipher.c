27ca3ce0164fe6600e362258db228408c4ba5d9c
shweta boraganve <Shweta.borganve@gmail.com>
2024-06-17 14:28:03 -0700

    caeser_cipher code

--- src/caesar_cipher.c
+++ src/caesar_cipher.c
@@ -1,14 +1,39 @@
 #include "caesar_cipher.h"
-
+#include <ctype.h>
+#include <string.h>
 void caesar_encrypt(char *message, int key) {
-    // Please remove this placeholder and implement Caesar encryption here
-    // Remember to remove or replace this comment as well
+    int i = 0;
+    char c;
+    
+    while (message[i] != '\0') {
+        c = message[i];
+
+        if (isalpha(c)) {
+            char base = islower(c)? 'a' : 'A';
+
+            message[i] = (c - base + key) % 26 + base;
+        }
+
+        i++;
+
+    }
     return;
 }
 
 void caesar_decrypt(char *message, int key) {
-    // Please remove this placeholder and implement Caesar decryption here
-    // Remember to remove or replace this comment as well
+    int i = 0;
+    char c;
+
+    while (message[i] != '\0') {
+        c = message[i];
+
+        if (isalpha(c)) {
+            char base = islower(c)? 'a' : 'A';
+            message[i] = (c - base - key) % 26 + base;
+        }
+        i++;
+    }
+    
     return;
 }
 
